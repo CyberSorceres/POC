0c161ea39574d0878a381c14d68ab13faf0b6a00
uiob <uiob1210@gmail.com>
2024-01-17 18:57:36 +0100

    Upload of demo test

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "poce952e3ba": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
---
+++ src/pages/_app.tsx
@@ -0,0 +1,19 @@
+import '@aws-amplify/ui-react/styles.css';
+import { Amplify } from 'aws-amplify';
+import awsconfig from '../aws-exports';
+import type { AppProps } from 'next/app';
+import { Authenticator } from '@aws-amplify/ui-react';
+
+Amplify.configure({
+  ...awsconfig,
+  // this lets you run Amplify code on the server-side in Next.js
+  ssr: true
+});
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <Authenticator>
+      <Component {...pageProps} />
+    </Authenticator>
+  );
+}
